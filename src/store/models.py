c00b7aeddada375a5c186d0b1ab9232989ada53d
Jatin Kumar Jain <31545613+Jatzj7@users.noreply.github.com>
2020-07-03 13:31:10 +0530

    changedModel

--- src/store/models.py
+++ src/store/models.py
@@ -18,46 +18,14 @@ class ProductDetail(models.Model):
     egg = models.BooleanField(default = True)
     image = models.ImageField(default='default.jpg',
                               upload_to='product_images')
+    price = models.DecimalField(max_digits=6, decimal_places=2,default = 199)
 
     def __str__(self):
         return self.name
 
-    @property
-    def get_price_1kg(self):
-        products = self.product_set.all()
-        price_list = [product.get_price for product in products if product.size == 0.5]
-        price = price_list[0]
-        return price
-
-    def get_price_size_all(self):
-        products = self.product_set.all()
-        p_size_list = [product.get_price_size for product in products]
-        return p_size_list
-    
-    
     def get_absolute_url(self):
         return reverse('product-detail', kwargs={'pk': self.pk , 'slug': self.name})
     
-
-class Product(models.Model):
-    productdetails = models.ForeignKey(ProductDetail, on_delete=models.CASCADE)
-    size = models.DecimalField(max_digits=4, decimal_places=2)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-
-    def __str__(self):
-        return str(self.productdetails.name + str(self.size))
-    
-    @property
-    def get_price(self):
-        price = int(self.price)
-        return price
-    
-    @property
-    def get_price_size(self):
-        size = self.size
-        price = int(self.price)
-        return size,price
-
 
 
 class Order(models.Model):
@@ -68,7 +36,7 @@ class Order(models.Model):
     transaction_id = models.CharField(max_length=100, null=True)
 
     def __str__(self):
-        return str(self.customer.username + str(self.id))
+        return str(self.customer.name + str(self.id))
     
     @property
     def get_cart_total(self):
@@ -84,7 +52,7 @@ class Order(models.Model):
 
 class OrderItem(models.Model):
     product = models.ForeignKey(
-        Product, on_delete=models.SET_NULL, null=True, blank=True)
+        ProductDetail, on_delete=models.SET_NULL, null=True, blank=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(default=timezone.now)
